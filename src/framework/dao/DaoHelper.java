53f5c90862d2d463d6ff62acfef028cf1b01f9bc
Elton Luis Kolling <elton@29sul.com.br>
2013-08-14 17:15:28 -0300

    Inicio da implementação de transactions.

--- src/framework/dao/DaoHelper.java
+++ src/framework/dao/DaoHelper.java
@@ -8,6 +8,8 @@ import java.sql.Statement;
 
 public class DaoHelper {
 
+    private static final ThreadLocal<Connection> context = new ThreadLocal<Connection>();
+    
     public Connection getConnection() throws SQLException {
         Connection conn = null;
         
@@ -17,6 +19,38 @@ public class DaoHelper {
             conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/video_locadora", "root", "");
         } catch (ClassNotFoundException e) {
             e.printStackTrace();
+        }
+        return conn;
+    }
+    
+    public void begingTransaction() throws SQLException {
+        Connection conn = getConnection();
+        conn.setAutoCommit(false);
+        context.set(conn);
+    }
+    
+    public void endTransaction() throws SQLException {
+        commit( getConnectionFromContext() );
+        releaseTransaction();
+    }
+    
+    public void releaseTransaction() throws SQLException {
+        Connection conn = getConnectionFromContext();
+        release(conn);
+        context.remove();
+    }
+    
+    public void commit(Connection conn) throws SQLException {
+        conn.commit();
+    }
+    
+    public Connection getConnectionFromContext() throws SQLException {
+        Connection conn = context.get();
+        
+        if ( conn == null ) {
+            throw new SQLException("Transação invalida.");
+        } else if (conn.isClosed()) {
+            throw new SQLException("Transação invalida. Conexção está fechada.");
         }
         
         return conn;
